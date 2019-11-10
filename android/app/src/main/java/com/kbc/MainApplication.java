48d8bc664b8b8ed8fd5ec78d16d303f9c2767dc9
abewartech <yusufarifin89@gmail.com>
2019-11-10 11:34:11 +0700

    AB : Fix bug

--- android/app/src/main/java/com/kbc/MainApplication.java
+++ android/app/src/main/java/com/kbc/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
-import com.henninghall.date_picker.DatePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
